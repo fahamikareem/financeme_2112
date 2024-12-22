a2a3591e77df4019d2e2c0f9031708b254d8240e
FahamiKareemEdavana <fahami.e@gmail.com>
2024-12-22 23:43:02 +0530

    updated script

--- agent_config.sh
+++ agent_config.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+PROJECT_DIRECTORY=FinanceMe_2112
+GIT_URL=https://github.com/fahamikareem/FinanceMe_2112.git
+
+# Update and install necessary packages
 sudo apt update
 sudo apt install git -y
-sudo apt install jdk-openjdk-17 -y
+sudo apt install openjdk-17-jdk -y  # Corrected package name for JDK 17
 sudo apt install maven -y
 
+# Clone or pull the repository
+if [ -d "$PROJECT_DIRECTORY" ]; then
+    echo "Directory $PROJECT_DIRECTORY exists. Pulling latest changes."
+    cd "$PROJECT_DIRECTORY" && git pull "$GIT_URL"
+else
+    echo "Directory $PROJECT_DIRECTORY does not exist. Cloning repository."
+    git clone "$GIT_URL"
+fi
